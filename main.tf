16ed04ba52145fe8612bea7c098a7e503e424bc1
bclnarsaiah <bclnarsaiah@gmail.com>
2020-08-13 23:03:33 +0530

    Auto Scaling group added

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+
 provider "aws" {
     region = "us-east-1"
 }
@@ -36,6 +39,45 @@ resource "aws_instance" "example" {
 output "public_ip" {
     value = aws_instance.example.public_ip
     description = "The public ip address of the web server"
+
+#Creation of Autoscaling group
+
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0c55b159cdfafe1f0"
+    instance_type = "t2.micro"
+    security_groups = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF 
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet_ids" "default" {
+    vpc_id = data.aws_vpc.default.id
+}
 
 
 
